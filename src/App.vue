a2c2a7e928323699f12e560b9441a26fd7d703c7
zhushaofeng <zhushaofeng19960409@gmail.com>
2017-12-27 08:51:42 +0800

    add daily

---
+++ src/App.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="daily">
+    <div class="daily-menu">
+      <div class="daily-menu-item" @click="handleToRecommend" :class="{ on: type === 'recommend' }">每日推荐</div>
+      <div class="daily-menu-item" @click="showThemes = !showThemes" :class="{ on: type === 'daily' }">主题日报</div>
+      <ul v-show="showThemes">
+        <li v-for="item in themes">
+          <a :class="{ on: item.id === themeId && type === 'daily' }">{{ item.name }}</a> 
+        </li>
+      </ul>
+    </div>
+    <div class="daily-list">
+      <item></item>
+    </div>
+    <daily-article></daily-article>
+  </div> 
+</template>
+<script>
+import $ from './libs/util'
+export default {
+  data () {
+    return {
+      themes: [],
+      showThemes: false,
+      type: 'recommend',
+      themeId: 0
+    }
+  },
+  methods: {
+    getThemes () {
+      $
+    }
+  }
+}
+</script>
