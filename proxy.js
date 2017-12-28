cb6f5127736f9f72554d71465298960d9b39be43
zhushaofeng <zhushaofeng19960409@gmail.com>
2017-12-28 08:43:11 +0800

    add some change

--- proxy.js
+++ proxy.js
@@ -2,9 +2,10 @@ const http = require('http');
 const request = require('request');
 
 const hostname = '127.0.0.1';
-const port = 8010;
-const imgPort = 8011;
+const port = 8012;
+const imgPort = 8013;
 
+// 创建一个 API 代理服务
 const apiServer = http.createServer((req, res) => {
   const url = 'http://news-at.zhihu.com/api/4' + req.url;
   const options = {
@@ -13,18 +14,21 @@ const apiServer = http.createServer((req, res) => {
 
   function callback(error, response, body) {
     if (!error && response.statusCode === 200) {
+      // 设置编码类型，否则中文会显示为乱码
       res.setHeader('Content-Type', 'text/plain;charset=UTF-8');
+      // 设置所有域允许跨域
       res.setHeader('Access-Control-Allow-Origin', '*');
+      // 返回代理后的内容
       res.end(body);
     }
   }
   request.get(options, callback);
 });
-
+// 监听 8012 端口
 apiServer.listen(port, hostname, () => {
   console.log(`接口代理运行在 http://${hostname}:${port}/`);
 });
-
+// 创建一个图片代理服务
 const imgServer = http.createServer((req, res) => {
   const url = req.url.split('/img/')[1];
   const options = {
@@ -42,7 +46,7 @@ const imgServer = http.createServer((req, res) => {
   }
   request.get(options, callback);
 });
-
+// 监听 8013 端口
 imgServer.listen(imgPort, hostname, () => {
   console.log(`图片代理运行在 http://${hostname}:${imgPort}/`);
 });
